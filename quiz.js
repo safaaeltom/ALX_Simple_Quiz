f4fecb1490f792d2fe80077ea5952e9392022786
Safaa <safaaeltom@gmail.com>
2025-12-03 00:10:49 +0300

    updated and added javascript file to repo

--- quiz.js
+++ quiz.js
@@ -1,21 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    function checkAnswer() {
-        let correctAnswer = "4";
+    function checkAnswer(event) {
+        event.preventDefault();
 
+        let correctAnswer = "4";
         let userAnswer = document.querySelector('input[name ="quiz"]:checked').value;
 
         if (userAnswer === correctAnswer) {
            document.getElementById("feedback").textContent = "Correct! Well done.";
         } else {
             document.getElementById("feedback").textContent = "That's incorrect. Try again!";
-           
-        document.getElementById("submit-answer"); 
+        }
+
+       let button = document.getElementById("submit-answer"); 
        button.addEventListener("click", checkAnswer);
-       function checkAnswer(event) {
-              event.preventDefault();
-        };
     }; 
 });
-
-
